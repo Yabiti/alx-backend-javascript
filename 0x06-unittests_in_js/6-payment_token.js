a2e5ef2c92893e3fc796e1f5af99e50cd52cb61e
Yabiti <yabetseyohannes@gmail.com>
2024-05-30 13:27:59 +0300

    .

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,8 @@
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+    if (success) {
+      resolve({data: 'Successful response from the API'});
+    }
+  });
+  
+  module.exports = getPaymentTokenFromAPI;
+  
